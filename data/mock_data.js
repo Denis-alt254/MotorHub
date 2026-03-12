4af63097ac00199deb34164c8eafcbd622d80ea2
Denis-alt254 <deniskipkurui2024@gmail.com>
2026-03-12 23:38:34 +0300

    modified mock data

--- data/mock_data.js
+++ data/mock_data.js
@@ -2,6 +2,9 @@ export const carData = [
     {
         engine: "2.0L Petrol Engine",
         fuelConsumption: "6.5L / 100km",
+        model: "Ford",
+        brand: "Mazda",
+        year: 2025,
         transmission: "Automatic",
         reliability: "4.5",
         topSpeed: "180km/h",
@@ -10,6 +13,9 @@ export const carData = [
     {
         engine: "2.0L Petrol Engine",
         fuelConsumption: "6.5L / 100km",
+        model: "Brado",
+        brand: "Toyota",
+        year: 2025,
         transmission: "Automatic",
         reliability: "4.5",
         topSpeed: "180km/h",
@@ -18,9 +24,25 @@ export const carData = [
     {
         engine: "2.0L Petrol Engine",
         fuelConsumption: "6.5L / 100km",
+        model: "V8",
+        brand: "Macendese",
+        year: 2025,
         transmission: "Automatic",
         reliability: "4.5",
         topSpeed: "180km/h",
         sparePartsAvailability: "Easily Available"
     }
 ]
+
+export const searchCar = (car) => {
+    //check for !car
+    if(!car) return []
+
+    //handle case-sensitive in search
+    const lowerCaseCar = car.toLowerCase()
+
+    return carData.filter(item => {
+        item.model.toLocaleLowerCase().includes(lowerCaseCar)
+        item.brand.toLocaleLowerCase().includes(lowerCaseCar)
+    })
+}
