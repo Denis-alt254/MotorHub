0d66f36dd0ee69050a418565ae588092622b9321
Denis-alt254 <deniskipkurui2024@gmail.com>
2026-03-12 19:30:20 +0300

    mock data

---
+++ data/mock_data.js
@@ -0,0 +1,26 @@
+export const carData = [
+    {
+        engine: "2.0L Petrol Engine",
+        fuelConsumption: "6.5L / 100km",
+        transmission: "Automatic",
+        reliability: "4.5",
+        topSpeed: "180km/h",
+        sparePartsAvailability: "Easily Available"
+    },
+    {
+        engine: "2.0L Petrol Engine",
+        fuelConsumption: "6.5L / 100km",
+        transmission: "Automatic",
+        reliability: "4.5",
+        topSpeed: "180km/h",
+        sparePartsAvailability: "Easily Available"
+    },
+    {
+        engine: "2.0L Petrol Engine",
+        fuelConsumption: "6.5L / 100km",
+        transmission: "Automatic",
+        reliability: "4.5",
+        topSpeed: "180km/h",
+        sparePartsAvailability: "Easily Available"
+    }
+]
